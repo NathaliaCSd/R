87fa548ee28e85f5c3d6396423447ff1b473d8d4
NathaliaCSd <csnathalia@estudante.ufscar.br>
2025-04-27 10:19:00 -0300

    Adicionado projeto sobre criacao de variaveis e armanezagem de valores em variaveis

---
+++ ArmazenandoValoresVariaveis.R
@@ -0,0 +1,25 @@
+#armazenando o valor em uma variavel
+#seta <-
+#ou
+#sinal de igual =
+
+x <- 5
+x
+
+y = 5
+y
+
+z = x + y
+z
+
+mynumber = 45.2
+v = mynumber * x
+v
+
+#criando novo x que ira sobresescrever o anteior (x = 5)
+x = x + 1
+x
+
+#funcao mostra todas as variaveis criadas pelo usuario neste arquivo
+ls()
+
